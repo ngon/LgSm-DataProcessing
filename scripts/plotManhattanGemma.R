ab4ffc17aaf2ba92c6c49beb897de2d2fcb37960
ngon <ngon@uchicago.edu>
2014-10-16 11:11:15 -0500

    altered settings in PlotManhattan
    changed colors, font sizes, etc.

--- scripts/plotManhattanGemma.R
+++ scripts/plotManhattanGemma.R
@@ -5,14 +5,14 @@ chrLens    <- c(0, cumsum(chrLens*1.0))
 labPos     <- (read.table('/home/shyamg/projects/Mouse/CFW/maps/chrLengths_mm10.txt')$V2)/2.0
 labPos     <- chrLens[1:19]+labPos
 
-plotManhattan <- function(trait, outfile=NULL, oddcolor="deepskyblue", evencolor="darkgreen", signifLine=6, signifColor="darkorange1", title=NULL) {
+plotManhattan <- function(trait, outfile=NULL, oddcolor="#ff6040", evencolor="#aab0be", signifLine=6, signifColor="#000000", title=NULL) {
     if (is.null(outfile)) {
         outfile <- paste0(trait, ".manhattan.jpg")
     }
     if (is.null(title)) {
         title = trait
     }
-    jpeg(file=outfile, quality=95, height=800, width=1600)
+    jpeg(file=outfile, quality=95, height=600, width=1400)
     pvals <- c()
     positions <- c()
     cols <- c()
@@ -29,12 +29,15 @@ plotManhattan <- function(trait, outfile=NULL, oddcolor="deepskyblue", evencolor
         }
         print(paste("Done with chromosome", chrom))
     }
-    plot(positions, pvals, col=cols, pch=19, cex=0.75, main=title, ylab='-log10(p-value)', xlab='Position', axes=F, frame.plot=T, cex.lab=1.1)
-    axis(2, cex.axis=1.2)
-    axis(1, at=labPos, labels=1:19, tick=F, cex.axis=1.2)
+    plot(positions, pvals, col=cols, pch=19, cex=0.78, main=title, ylab='-log10(p-value)', xlab='Position', axes=F, frame.plot=T, cex.lab=1.7, cex.main=1.8, family="Lucida Sans", font.main=1)
+    axis(2, cex.axis=1.5)
+    axis(1, at=labPos, labels=1:19, tick=F, cex.axis=1.5)
     abline(h=signifLine, col=signifColor, lty=2)
     dev.off()
 }
+
+
+
 
 plotZoom <- function(trait, chrom, start, end, outfile=NULL, dotColor="firebrick", signifLine=6, signifColor="darkorange1", title=NULL, scaling=1e6, scalename="(Mb)") {
     if (is.null(outfile)) {
@@ -61,9 +64,9 @@ plotZoom <- function(trait, chrom, start, end, outfile=NULL, dotColor="firebrick
 }
 
 
-traits <- c("cpp8.t", "sens", "act1.t", "act2.t", "act3.t", "act4.t", "act5.t", "act8.t", "avg.ppi", "startle", "glucose", "wild")
-titles <- c("Total time spent on meth paired side\non day 8", "Total activity on day 1", "Total activity on day 2", "Total activity on day 3", "Total activity on day 4", "Total activity on day 5", "Total activity on day 8", "Average prepulse inhibition", "Startle response", "Blood glucose", "Wildness")
-outfiles <- paste0("/home/shyamg/projects/Mouse/AIL/LgSm-DataProcessing/figures/", traits, ".manhattan.jpg")
+traits <- c("cpp8.t", "act2.t", "act3.t", "act4.t", "act8.t", "startle", "wild", "ppi3", "ppi6", "ppi12", "habituation", "cpp.diff", "cpp8.1")
+titles <- c("Time spent on meth paired side on day 8 (30 min)", "Day 2 activity (1 mg/kg meth)", "Day 3 activity (saline)", "Day 4 activity (1 mg/kg meth)", "Day 8 activity (saline)", "Startle response", "Wildness (number of escapes during cpp)", "Prepulse inhibition (73 dB)", "Prepulse inhibition (76 dB)", "Prepulse inhibition (82 dB)", "Habituation to acoustic startle", "Change in preference for meth paired side", "Time spent on meth paired side on day 8 (5 min)")
+outfiles <- paste0("/home/shyamg/projects/Mouse/AIL/LgSm-DataProcessing/figures/manhattan", traits, ".manhattan.jpg")
 
 for (index in 1:12) {
     print(paste("Starting trait", traits[index]))
