f4dcc2452e0e03c047bea79981826d27a19bdb57
ngon <ngon@uchicago.edu>
2015-02-19 17:26:23 -0600

    save r scripts

--- figures/manhattan/plotQTL.R
+++ figures/manhattan/plotQTL.R
@@ -13,41 +13,47 @@ labPos     <- read.table("chrLengths_mm10.txt")$V2/2.0
 labPos     <- chrLens[1:19]+labPos
 
 
-traits <- c(#"cpp.diff", 
+traits <- c("cpp.diff", 
             #"cpp8.1", 
-            "cpp8.t",
+            #"cpp8.t",
+            "act1.t",
             "act2.t", 
             #"act4.t", 
-            "act5.t",
+            #"act5.t",
             #"act8.t", 
             #"sens", 
             #"startle", 
             #"wild", 
             #"ppi3", 
-            #"ppi6", 
-            "ppi12", 
+            "ppi6"
+            #"ppi12"
             #"habituation", 
-            #"glucose")
+            #"glucose"
+            )
 
-titles <- c(#"Change in preference for meth paired side", 
+titles <- c("Change in preference for meth paired side", 
             #"Time spent on meth paired side on day 8 (5 min)", 
-            "Time spent on meth paired side on day 8 (30 min)", 
-            #"Locomotor response to novelty (Day 1 activity)",
-            "Day 2 activity (1 mg/kg meth)", 
+            #"Time spent on meth paired side on day 8 (30 min)", 
+            "Locomotor response to novelty (Day 1 activity)",
+            "Locomotor response to meth (Day 2 activity)", 
             #"Day 4 activity (1 mg/kg meth)", 
-            "Day 5 activity (saline)",
+            #"Day 5 activity (saline)",
             #"Day 8 activity (saline)",
             #"Locomotor sensitization to meth", 
-            #"Acoustic startle response", 
+            #"Acoustic startle reflex", 
             #"Wildness (number of escapes during CPP)", 
             #"Prepulse inhibition (73 dB)",
-            #"Prepulse inhibition (76 dB)", 
-            "Prepulse inhibition (82 dB)", 
+            "Prepulse inhibition (76 dB)" 
+           # "Prepulse inhibition (82 dB)",
             #"Habituation to acoustic startle",
-            #"Blood glucose levels after a 4h fast")
+            #"Blood glucose levels after a 4h fast"
+            )
 
 outfiles <- paste0(traits, ".manhattan.png")
 
+traits<- c("startle", "ppi12")
+titles<- c("Acoustic startle reflex", "Prepulse inhibition (82 dB)")
+outfiles <- paste0(traits, ".manhattan.png")
 
 for (index in 1:1) {
         print(paste("Starting trait", traits[index]))
@@ -58,9 +64,10 @@ for (index in 1:1) {
 
 
 
+
 plotManhattan <- function(trait, outfile=NULL, 
                           oddcolor="#ff6040", evencolor="#aab0be", 
-                          signifLine=6, signifColor="#000000", title=NULL) {
+                          signifLine=5, signifColor="#000000", title=NULL) {
         if (is.null(outfile)) {
                 outfile <- paste0(trait, ".manhattan.png")
         }
@@ -86,39 +93,91 @@ plotManhattan <- function(trait, outfile=NULL,
                 }
                 print(paste("Done with chromosome", chrom))
         }
-        plot(positions, pvals, col=cols, pch=19, cex=0.78, main=title, ylab='-log10(p-value)', xlab='Position', axes=F, frame.plot=T, cex.lab=1.7, cex.main=1.8, font.main=1)
+        plot(positions, pvals, col=cols, pch=19, cex=0.78, main=title, ylab='-log10(p-value)', xlab='Position', axes=F, frame.plot=T, cex.lab=1.7, cex.main=1.8, font.main=1, ylim =c(0,7) )
         axis(2, cex.axis=1.5)
         axis(1, at=labPos, labels=1:19, tick=F, cex.axis=1.5)
         abline(h=signifLine, col=signifColor, lty=2)
         dev.off()
 }
 
+__________________________________________
+
+### zoom plot for cpp.diff - ch4
+
+# cppdiff.ch4 <- read.table("cpp.diff.chr4.assoc.txt", sep="\t", header=T )
+# test<-cppdiff.ch4[order(cppdiff.ch4$p_lrt),]
+# plotZoom(trait="cpp.diff", chrom=4, start=89803883, end=114867413)
+# 
+# ppi6.ch8 <- read.table("ppi6.chr8.assoc.txt", sep="\t", header=T )
+# test<-ppi6.ch8[order(ppi6.ch8$ps),]
+# plotZoom(trait="ppi6", chrom=8, start=69822096, end=95001306)
+# 
+# ppi12.ch8 <- read.table("ppi12.chr8.assoc.txt", sep="\t", header=T )
+# test<-ppi12.ch8[order(ppi12.ch8$ps),]
+# plotZoom(trait="ppi12", chrom=8, start=69822096, end=95001306)
+# 
+# startle17 <- read.table("startle.chr17.assoc.txt", sep="\t", header=T )
+# test<-startle17[order(startle17$ps),]
+# plotZoom(trait="startle", chrom=17, start=18998527, end=35964216)
+# 
+# startle7 <- read.table("startle.chr7.assoc.txt", sep="\t", header=T )
+# test<-startle7[order(startle7$ps),]
+# plotZoom(trait="startle", chrom=7, start=75001287, end=84997578)
+# 
+# cpp5min.7 <- read.table("cpp8.1.chr7.assoc.txt", sep="\t", header=T )
+# test<-cpp5min.7[order(cpp5min.7$ps),]
+# plotZoom(trait="cpp8.1", chrom=7, start=75001287, end=99997517)
+# 
+# act1t7 <- read.table("act1.t.chr7.assoc.txt", sep="\t", header=T )
+# test<-act1t7[order(act1t7$ps),]
+# plotZoom(trait="act1.t", chrom=7, start=75001287, end=99997517)
+# 
+# act1t17 <- read.table("act1.t.chr17.assoc.txt", sep="\t", header=T )
+# test<-act1t17[order(act1t17$ps),]
+# plotZoom(trait="act1.t", chrom=17, start=, end=)
+# 
+# act2t7 <- read.table("act2.t.chr7.assoc.txt", sep="\t", header=T )
+# test<-act2t7[order(act2t7$ps),]
+# plotZoom(trait="act2.t", chrom=7, start=30000032, end=45847045)
+# 
+# act2t17 <- read.table("act2.t.chr17.assoc.txt", sep="\t", header=T )
+# test<-act2t17[order(act2t17$ps),]
+# plotZoom(trait="act2.t", chrom=17, start=75999608, end= 85035207)
+
+act2t4 <- read.table("act2.t.chr4.assoc.txt", sep="\t", header=T )
+test<-act2t4[order(act2t4$ps),]
+plotZoom(trait="act2.t", chrom=4, start=127158422, end=134999602)
+
+act5t4 <- read.table("act5.t.chr4.assoc.txt", sep="\t", header=T )
+test<-act5t4[order(act5t4$ps),]
+plotZoom(trait="act5.t", chrom=4, start=127158422, end=134999602)
 
 
 
-# plotZoom <- function(trait, chrom, start, end, outfile=NULL, dotColor="firebrick", signifLine=6, signifColor="darkorange1", title=NULL, scaling=1e6, scalename="(Mb)") {
-#     if (is.null(outfile)) {
-#         outfile <- paste0(trait, ".chr", chrom, ".", as.character(start), ".", as.character(end), ".zoom.jpg")
-#     }
-#     if (is.null(title)) {
-#         title = trait
-#     }
-#     jpeg(file=outfile, quality=95)
-#     filename <- paste0("/home/shyamg/projects/Mouse/AIL/LgSm-DataProcessing/output/", trait, ".chr", chrom, ".assoc.txt")
-#     print(paste("Starting chromosome", chrom))
-#     output <- read.table(filename, header=T)
-#     pvals <- -log10(output$p_lrt)
-#     positions <- output$ps/scaling
-#     print(paste("Done with chromosome", chrom))
-#     ### Code to plot
-#     start = start/scaling
-#     end = end/scaling
-#     plot(positions, pvals, col=dotColor, pch=19, cex=0.75, main=title, ylab='-log10(p-value)', xlab=paste('Position', scalename), axes=F, frame.plot=T, cex.lab=1.1, xlim=c(start, end))
-#     axis(2, cex.axis=1.2)
-#     axis(1, cex.axis=1.2)
-#     abline(h=signifLine, col=signifColor, lty=2)
-#     dev.off()        
-# }
+
+plotZoom <- function(trait, chrom, start, end, outfile=NULL, dotColor="#aab0be", signifLine=5, signifColor="#000000", title=NULL, scaling=1e6, scalename="(Mb)") {
+    if (is.null(outfile)) {
+        outfile <- paste0(trait, ".chr", chrom, ".", as.character(start), ".", as.character(end), ".zoom.jpg")
+    }
+    if (is.null(title)) {
+        title = trait
+    }
+    jpeg(file=outfile, quality=95)
+    filename <- paste0(trait, ".chr", chrom, ".assoc.txt")
+    print(paste("Starting chromosome", chrom))
+    output <- read.table(filename, header=T)
+    pvals <- -log10(output$p_lrt)
+    positions <- output$ps/scaling
+    print(paste("Done with chromosome", chrom))
+    ### Code to plot
+    start = start/scaling
+    end = end/scaling
+    plot(positions, pvals, col=dotColor, pch=19, cex=0.75, main=title, ylab='-log10(p-value)', xlab=paste('Position', scalename), axes=F, frame.plot=T, cex.lab=1.1, xlim=c(start, end))
+    axis(2, cex.axis=1.2)
+    axis(1, cex.axis=1.2)
+    abline(h=signifLine, col=signifColor, lty=2)
+    dev.off()        
+}
 
 
 
