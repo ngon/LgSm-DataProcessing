68f359aaab48543501f9bdd4790d1fa35ef2a3de
ngon <ngon@uchicago.edu>
2015-04-16 16:48:38 -0500

    fixed mafandhet script

---
+++ cookieTime/gw.summaries.R
@@ -0,0 +1,111 @@
+setwd("/group/palmer-lab/AIL/GBS/genoSummaries")
+load("/group/palmer-lab/AIL/GBS/genoSummaries/empRows.Rdata")
+library("ggplot2")
+
+### genome wide maf --------------------------------------------------------------
+### all snps
+gw.maf <- function(file) {
+    geno <- read.table(file, header=F, as.is=T)[-c(1:4)]
+    freq <- cbind((rowMeans(geno, na.rm = T)/2),
+                   1-(rowMeans(geno, na.rm=T)/2))
+    maf <- round(apply(freq, 1, min), digits=2)
+    return(maf)
+}
+
+### empirical snps
+gw.emaf <- function(file, e=emp.rows){
+
+    geno <- read.table(file, header=F, as.is=T)[-c(1:4)]
+    freq <- cbind((rowMeans(geno, na.rm = T)/2),
+                  1-(rowMeans(geno, na.rm=T)/2))
+    maf <- round(apply(freq, 1, min), digits=2)
+    chrname <- substr(basename(file), 1, nchar(basename(file))-16)
+    #chrname <- substr(basename(file), 1, nchar(basename(file))-4)
+    empR <- e[[chrname]]
+    emp.maf <- sort(maf[seq_along(maf) %in% empR])
+    return(emp.maf)
+}
+
+### plot mafs
+chromosomes <- paste0("chr", 1:19)
+filenames <- list()
+for (i in chromosomes){
+    filenames[i] <- paste0("../dosage/", i, ".filtered.dosage")
+}
+
+all_maf <- data.frame(table(sort(unlist(lapply(filenames, gw.maf)))))
+all_maf$Var1 <- as.numeric(as.character(all_maf$Var1))
+emp_maf <- data.frame(table(sort(unlist(lapply(filenames, gw.emaf, e=empRows)))))
+emp_maf$Var1 <- as.numeric(as.character(emp_maf$Var1))
+
+pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/gw_maf.pdf",
+    height=4, width=4, colormodel="cmyk")
+
+ggplot(data=all_maf, aes(x=Var1, y=Freq/sum(Freq))) +
+    geom_bar(stat="identity", fill="grey77", color="black") +
+    geom_bar(data=emp_maf, aes(x=Var1, y=Freq/sum(Freq)), stat="identity",
+                               fill="grey25", alpha=0.5, color="black") +
+    scale_x_continuous(breaks=seq(0,0.5, 0.05))+
+    xlab("Minor allele frequency") +
+    ylab("Proportion of SNPs") +
+    theme_bw() +
+    theme(axis.title.x=element_text(size=12),
+          axis.title.y=element_text(size=12),
+          axis.text.x=element_text(size=10),
+          axis.text.y=element_text(size=10))
+
+dev.off()
+
+
+
+
+### genome wide het --------------------------------------------------------------
+gw.het <- function(file, upper=1.2, lower=0.8) {
+    geno <- read.table(file, header=F, as.is=T)[-c(1:3)]
+    hetsites <- geno <= upper & geno >= lower
+    het.snp <- round(apply(hetsites, 1, mean), digits=2)
+    return(het.snp)
+    }
+
+gw.ehet <- function(file, upper=1.2, lower=0.8, e=emp.rows){
+    geno <- read.table(file, header=F, as.is=T)[-c(1:3)]
+    hetsites <- geno <= upper & geno >= lower
+    het.snp <- round(apply(hetsites, 1, mean), digits=2)
+    chrname <- substr(basename(file), 1, nchar(basename(file))-16)
+    #chrname <- substr(basename(file), 1, nchar(basename(file))-4)
+    empR <- e[[chrname]]
+    emp.het <- sort(het.snp[seq_along(het.snp) %in% empR])
+    return(emp.het)
+}
+
+
+
+all_het <- data.frame(table(sort(unlist(lapply(filenames, gw.het)))))
+all_het$Var1 <- as.numeric(as.character(all_het$Var1))
+emp_het <- data.frame(table(sort(unlist(lapply(filenames, gw.ehet, e=empRows)))))
+emp_het$Var1 <- as.numeric(as.character(emp_het$Var1))
+
+pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/gw_het.pdf",
+    height=4, width=4, colormodel="cmyk")
+
+ggplot(data=all_het, aes(x=Var1, y=Freq/sum(Freq))) +
+    geom_bar(stat="identity", fill="grey77", color="black") +
+    geom_bar(data=emp_het, aes(x=Var1, y=Freq/sum(Freq)), stat="identity",
+             fill="grey25", alpha=0.5, color="black") +
+    scale_x_continuous(breaks=seq(0,0.5, 0.05))+
+    xlab("Average SNP heterozygosity") +
+    ylab("Proportion of SNPs") +
+    theme_bw() +
+    theme(axis.title.x=element_text(size=12),
+          axis.title.y=element_text(size=12),
+          axis.text.x=element_text(size=10),
+          axis.text.y=element_text(size=10))
+
+dev.off()
+
+
+
+
+
+
+
--- cookieTime/maf.and.het.R
+++ cookieTime/maf.and.het.R
@@ -1,8 +1,8 @@
 # load positions of empirical snps (a list called empRows)
 setwd("/group/palmer-lab/AIL/GBS/genoSummaries")
 load("/group/palmer-lab/AIL/GBS/genoSummaries/empRows.Rdata")
-source("/group/palmer-lab/AIL/LgSm-DataProcessing/cookieTime/multiplot.R")
-library("ggplot2")
+#source("/group/palmer-lab/AIL/LgSm-DataProcessing/cookieTime/multiplot.R")
+#library("ggplot2")
 
 ### get.empirical.data ----------------------------------------------------
 get.empirical.data <- function(file, e=emp.rows, x=maf, y=het.snp){
@@ -11,12 +11,14 @@ get.empirical.data <- function(file, e=emp.rows, x=maf, y=het.snp){
 
     chrname             <- substr(basename(file), 1, nchar(basename(file))-16)
     empR                <- e[[chrname]]
-    emp.maf             <- data.frame(table(sort(x[seq_along(x) %in% empR])))
-    names(emp.maf)[1]   <- "emp.maf"
-    emp.het             <- data.frame(table(sort(y[seq_along(y) %in% empR])))
-    names(emp.het)[1]   <- "emp.het.snp"
+    emp.maf             <- sort(x[seq_along(x) %in% empR])
+    empmaf.hist         <- hist(emp.maf[1], breaks=seq(0,0.5, 0.05), plot=F,
+                        include.lowest=T)
+    emp.het             <- sort(y[seq_along(y) %in% empR])
+    emphet.hist         <- hist(emp.het[1], breaks=seq(0,0.5, 0.05), plot=F,
+                        include.lowest=T)
 
-    list(emp.maf=emp.maf, emp.het=emp.het)
+    list(empmaf.hist=empmaf,hist, emphet.hist=emphet.hist)
 }
 
 ### maf and het --------------------------------------------------------------
@@ -28,117 +30,109 @@ maf.and.het <- function(file, upper=1.2, lower=0.8, emp.rows=NULL) {
     print("Calculating minor allele frequencies...")
             freq        <- cbind( (rowMeans(geno, na.rm = T)/2),
                             1-(rowMeans(geno, na.rm=T)/2)  )
-            maf         <- round(apply(freq, 1, min), digits=1)
+            maf         <- round(apply(freq, 1, min), digits=2)
+            maf.hist    <- hist(maf, breaks=seq(0,0.5, 0.05), plot=F,
+                            include.lowest=T)
 
     print("Calculating average heterozygosity...")
             hetsites    <- geno <= upper & geno >= lower
             het.mouse   <- apply(hetsites, 2, mean)
-            het.snp     <- round(apply(hetsites, 1, mean), digits=1)
+            het.snp     <- round(apply(hetsites, 1, mean), digits=2)
+            hetsnp.hist <- hist(het.snp, breaks=seq(0,0.5, 0.05), plot=F,
+                            include.lowest=T)
 
     if(!is.null(emp.rows)){
-        empStats          <- get.empirical.data(file=file, e=emp.rows, x=maf, y=het.snp)
-
-        maf               <- data.frame(table(sort(maf)))
-        names(maf)[1]     <- "maf"
-        het.snp           <- data.frame(table(sort(het.snp)))
-        names(het.snp)[1] <- "het.snp"
-        result  <- list(maf, het.snp, het.mouse, empStats["emp.maf"], empStats["emp.het"])
-
+        empStats <- get.empirical.data(file=file, e=emp.rows, x=maf, y=het.snp)
+        result<- list(maf.hist, hetsnp.hist, het.mouse,
+                      empStats["empmaf.hist"], empStats["emphet.hist"])
     } else {
-        maf               <- data.frame(table(sort(maf)))
-        names(maf)[1]     <- "maf"
-        het.snp           <- data.frame(table(sort(het.snp)))
-        names(het.snp)[1] <- "het.snp"
         result<-  list(maf, het.snp, het.mouse)
     }
     return(result)
 
 }
 
+### plotting functions ------------------------------------------------------
+plot.maf.snp <- function(all, emp) {
+    plot(x=all[[1]], xlab=paste0("Minor allele frequency on ", chr),
+         ylab="Number of SNPs", main=NULL, cex.lab=1, cex.axis=.9,
+         col=rgb(255,193,37, maxColorValue = 255))
 
-## get a list of genotype file names
+    plot(x=emp[[1]]$empmaf.hist, col=rgb(255,69,0,128, maxColorValue = 255), add=T)
+
+    legend(x=0, y=max(all[[1]]$counts), legend=c("All", "GBS"), bty="n",
+           cex=0.8, fill=c(rgb(255,193,37, maxColorValue=255),
+                           rgb(255,69,0,150, maxColorValue=255)))
+    box()
+}
+
+
+plot.het.snp <- function(all, emp) {
+    plot(x=all[[1]], xlab=paste0("Average heterozygosity on ", chr),
+         ylab="Number of SNPs", main=NULL, cex.lab=1, cex.axis=.9,
+         col=rgb(255,193,37, maxColorValue = 255))
+
+    plot(x=emp[[1]]$emphet.hist, col=rgb(255,69,0,128, maxColorValue = 255), add=T)
+
+    legend(x=0, y=max(all[[1]]$counts), legend=c("All", "GBS"), bty="n",
+           cex=0.8, fill=c(rgb(255,193,37, maxColorValue=255),
+                           rgb(255,69,0,150, maxColorValue=255)))
+    box()
+}
+
+plot.het.mouse <- function(mice) {
+    hist(x=mice[[1]], xlab=paste0("Average heterozygosity on ", chr),
+         ylab="Number of mice", main=NULL, cex.lab=1, cex.axis=.9,
+         col=rgb(255,193,37, maxColorValue = 255))
+    box()
+}
+
+
+### get data-----------------------------------------------------------------
 chromosomes <- paste0("chr", 1:19)
 filenames <- list()
 for (i in chromosomes){
     filenames[i] <- paste0("../dosage/", i, ".filtered.dosage")
 }
+data <- lapply(filenames, maf.and.het, emp.rows=empRows)
 
-## get data
-snpInfo <- lapply(filenames, maf.and.het, emp.rows=empRows)
 
-## plot data
-mafplots <- list()
-hetplots <- list()
-mouseplots <- list()
-
-data <- snpInfo
-
+## plot maf.snp
+pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/maf_chr.pdf",
+    height=12, width=14, colormodel="cmyk")
+par(mfrow=c(5,4))
 for(chr in chromosomes){
-    print("Plotting MAF for each chromosome....")
-    all <- data.frame(data[[chr]][1])
-    names(all)[1:2] <-c("maf", "Freq")
-    emp <- data.frame(data[[chr]][4])
-    names(emp)[1:2] <-c("maf", "Freq")
-
-    mafplots[[chr]] <- ggplot(data=all, aes(x=maf, y=Freq/sum(Freq))) +
-        geom_bar(stat="identity", fill="goldenrod1", color="black") +
-        geom_bar(data=emp, aes(x=maf, y=Freq/sum(Freq)),
-                 stat="identity", fill="orangered", alpha=0.5, color="black") +
-        xlab(paste0("Minor allele frequency on ", chr)) +
-        ylab("Proportion of SNPs") +
-        theme_bw() +
-        theme(axis.title.x=element_text(size=10),
-              axis.title.y=element_text(size=11),
-              axis.text.x=element_text(size=10),
-              axis.text.y=element_text(size=10))
-
-    print("Plotting SNP heterozygosity for each chromosome....")
-    all <- data.frame(data[[chr]][2])
-    names(all)[1:2] <-c("het", "Freq")
-    emp <- data.frame(data[[chr]][5])
-    names(emp)[1:2] <-c("het", "Freq")
-
-    hetplots[[chr]] <- ggplot(data=all, aes(x=het, y=Freq/sum(Freq))) +
-        geom_bar(stat="identity", fill="goldenrod1", color="black") +
-        geom_bar(data=emp, aes(x=het, y=Freq/sum(Freq)),
-                 stat="identity", fill="orangered", alpha=0.5, color="black") +
-        xlab(paste0("Average heterozygosity on ", chr)) +
-        ylab("Proportion of SNPs") +
-        theme_bw() +
-        theme(axis.title.x=element_text(size=10),
-              axis.title.y=element_text(size=11),
-              axis.text.x=element_text(size=10),
-              axis.text.y=element_text(size=10))
-
-    print("Plotting mouse heterozygosity for each chromosome....")
-    all <- as.data.frame(unlist(data[[chr]][3]))
-    names(all)[1] <- "het"
-
-    mouseplots[[chr]] <- ggplot(data=all, aes(x=het)) +
-        geom_histogram(fill="goldenrod1", color="black", binwidth=0.1) +
-        xlab(paste0("Average heterozygosity on ", chr)) +
-        ylab("Proportion of mice") +
-        theme_bw() +
-        theme(axis.title.x=element_text(size=10),
-              axis.title.y=element_text(size=11),
-              axis.text.x=element_text(size=10),
-              axis.text.y=element_text(size=10))
-
+    all <- data[[chr]][1]
+    emp <- data[[chr]][4]
+    plot.maf.snp(all, emp)
 }
-
-
-
-pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/mafChromosomes.pdf",
-    height=12, width=10, colormodel="cmyk")
-multiplot(plotlist=mafplots, cols=4)
 dev.off()
 
-pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/hetsnpChromosomes.pdf",
-    height=12, width=10, colormodel="cmyk")
-multiplot(plotlist=hetplots, cols=4)
+## plot het.snp
+pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/snphet_chr.pdf",
+    height=12, width=14, colormodel="cmyk")
+par(mfrow=c(5,4))
+for(chr in chromosomes){
+    all <- data[[chr]][2]
+    emp <- data[[chr]][5]
+    plot.het.snp(all, emp)
+}
 dev.off()
 
-pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/hetmouseChromosomes.pdf",
-    height=10, width=8, colormodel="cmyk")
-multiplot(plotlist=mouseplots, cols=5)
+## plot het mouse
+pdf(file="/group/palmer-lab/AIL/LgSm-DataProcessing/figures/mousehet_chr.pdf",
+    height=15, width=12, colormodel="cmyk")
+par(mfrow=c(5,4))
+for(chr in chromosomes){
+    mice <- data[[chr]][3]
+    plot.het.mouse(mice)
+}
 dev.off()
+
+
+
+
+
+
+
+
