d51d9abf884107e5c5e8051ee0b5fc237d00a72f
Gonzales <ngonzales@cri12in02.cri.uchicago.edu>
2015-05-18 11:40:16 -0500

    testing square manhattan cM plot

---
+++ append.cM.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#### scheduler directives ####
+
+#PBS -N append.cM.emp
+#PBS -S /bin/bash
+#PBS -l walltime=12:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=20gb
+#PBS -j oe
+
+
+#### execute job #####
+
+module load R/3.1.0
+cd /group/palmer-lab/AIL/qtlmapping
+R CMD BATCH /group/palmer-lab/AIL/LgSm-DataProcessing/cookieTime/append.cM.R
