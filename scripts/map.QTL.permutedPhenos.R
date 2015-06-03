8d3d5a4dbff2ae4586641da8953108c01e16a63f
ngon <ngon@uchicago.edu>
2015-06-03 13:40:31 -0500

    removed header from permCov files

--- scripts/map.QTL.permutedPhenos.R
+++ scripts/map.QTL.permutedPhenos.R
@@ -12,53 +12,50 @@ MAF=0.05
 # 51717-19 changed to 51717 (double ear tag)
 # 51348 changed to 51349 (inferred from ped checks - typo? - double check using sex(genotypes on X).
 geno.samples <- read.table("/group/palmer-lab/AIL/GBS/dosage/genotyped.samples.txt", as.is=TRUE)
-geno.samples[which(geno.samples$V1 == "54109_11"), 1] <- "54109"
-geno.samples[which(geno.samples$V1 == "51717-19"), 1] <- "51717"
-geno.samples[which(geno.samples$V1 == "51348"), 1]    <- "51349"
+# geno.samples[which(geno.samples$V1 == "54109_11"), 1] <- "54109"
+# geno.samples[which(geno.samples$V1 == "51717-19"), 1] <- "51717"
+# geno.samples[which(geno.samples$V1 == "51348"), 1]    <- "51349"
 names(geno.samples) <- c("id")
+ids <- geno.samples$id
 
 # READ TAB DELIMITED FILE WITH PERMUTED PHENOTYPES
 # Sex is an indicator variable; M=0 and F=1
-pheno <- read.table("./permutedPhenotypes.txt", sep="\t", header=T, as.is=T)
+pheno <- read.table("./permutedPhenotypes.txt", sep="\t", header=T, as.is=T, nrows=2)
 #covars <- read.table("./covariates.orm.txt", sep="\t", header=T, as.is=T)
 pheno.names <- names(pheno)
-ids<- read.table("./dataFiles/", header=T, as.is=T)[2]
-names(ids)[1] <- "id"
-pheno <- cbind(ids[1], pheno)
+
+traits.to.perm <- c("act1.t", "act5.t", "startle", "wild.binary",  "ppi12.logit", "tail")
+#pheno <- cbind(ids[1], pheno)
 
 #### EXTRACT DATA FOR GENOTYPED SAMPLES ------------------------------------
 ### Generate phenotype data for all samples with genotypes
-permPheno.allgeno <- merge(geno.samples, pheno, all.x=TRUE)
-write.table(permPheno.allgeno, file="permPhenos.allgeno.txt",
-            quote=FALSE, sep="\t", row.names=FALSE, col.names=FALSE)
-
+# permPheno.allgeno <- merge(geno.samples, pheno, all.x=TRUE)
+# write.table(permPheno.allgeno, file="permPhenos.allgeno.txt",
+#             quote=FALSE, sep="\t", row.names=FALSE, col.names=FALSE)
 
 ### Make covariate file with all ids in genotype file
-traits.to.perm <- c("act1.t", "act2.t", "act4.t", "act5.t", "act8.t", "cpp.diff",
-                    "sc8.t", "sc1.t", "startle", "wild.binary", "ppi6.logit",
-                    "ppi12.logit", "tail","glucose")
 
-get.filenames <- function(trait){
-filenames <- c()
-for (i in 1:5){
-filenames[i] <- paste0("/group/palmer-lab/AIL/qtlmapping/covariates/permCovs/perm",
-                    i, ".", trait, ".cov.txt")
-}
-return(filenames)
-}
-files <- lapply(traits.to.perm, get.filenames)
-files <- unlist(files)
+# get.filenames <- function(trait){
+# filenames <- c()
+# for (i in 1:100){
+# filenames[i] <- paste0("/group/palmer-lab/AIL/qtlmapping/covariates/permCovs/perm",
+#                     i, ".", trait, ".cov.txt")
+# }
+# return(filenames)
+# }
+# files <- lapply(traits.to.perm, get.filenames)
+# files <- unlist(files)
 
-merge.covars <- function(covFile, genoFile, ids){
-    covariate <- read.table(covFile, as.is=T, header=T)
-    covariate <- cbind(ids, covariate)
-    covariate <- merge(genoFile, covariate, header=T, all.x=TRUE)
-    covariate$one <- 1
-    write.table(covariate[-1], file=paste0(covFile,"2"), sep="\t", row.names=F, col.names=F, quote=F)
-}
-
-lapply(files, merge.covars, genoFile=geno.samples, ids=ids)
-
+# merge.covars <- function(covFile, genoFile, ids){
+#     covariate <- read.table(covFile, as.is=T, header=T)
+#     covariate <- cbind(ids, covariate)
+#     covariate <- merge(genoFile, covariate, header=T, all.x=TRUE)
+#     covariate$one <- 1
+#     write.table(covariate[-1], file=paste0(covFile,"2"), sep="\t", row.names=F, col.names=F, quote=F)
+# }
+#
+# lapply(files, merge.covars, genoFile=geno.samples, ids=ids)
+#
 
 ### MAKE SCRIPT TO RUN GEMMA FOR CHOSEN TRAIT ----------------------------------------------
 
@@ -67,15 +64,12 @@ for (pheno in pheno.names) {
 
     index.pheno     <- which(pheno.names == pheno)
     chosen.covars   <- paste0("/group/palmer-lab/AIL/qtlmapping/covariates/permCovs/",
-                              pheno, ".cov.txt2")
+                              pheno, ".cov.txt")
 
 
     for (chrom in 1:19) {
         cmds <- c(cmds, paste0("gemma -g /group/palmer-lab/AIL/GBS/dosage/onlyEmpirical/chr", chrom,
-                               ".filtered.dosage -p /group/palmer-lab/AIL/LgSm-DataProcessing/permPhenos.allgeno.txt -k /group/palmer-lab/AIL/qtlmapping/kinship/onlyEmpirical/chrNot",
-                               chrom,".cXX.txt -a /group/palmer-lab/AIL/GBS/dosage/onlyEmpirical/chr",
-                               chrom, ".filtered.snpinfo -c ", chosen.covars,
-                               " -lmm 2 -maf ", MAF, " -o ", pheno, ".chr", chrom, " -n ", index.pheno))
+                               ".filtered.dosage -p /group/palmer-lab/AIL/LgSm-DataProcessing/permPhenos.allgeno.txt -k /group/palmer-lab/AIL/qtlmapping/kinship/onlyEmpirical/identity.kinship -a /group/palmer-lab/AIL/GBS/dosage/onlyEmpirical/chr",chrom, ".filtered.snpinfo -c ", chosen.covars," -lmm 2 -maf ", MAF, " -o ", pheno, ".chr", chrom, " -n ", index.pheno))
 }
 write.table(cmds, file=paste0("/group/palmer-lab/AIL/code/gemma.permPheno.emp.cmds"),
             row.names=F, col.names=F, quote=F)
