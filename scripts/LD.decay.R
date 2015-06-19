992237dca2b42b2e2ae1ebcae851fcced24db8b7
ngon <ngon@uchicago.edu>
2015-06-19 13:34:02 -0500

    hopefully fixed problem with pdf

--- scripts/LD.decay.R
+++ scripts/LD.decay.R
@@ -3,7 +3,7 @@ files <- paste0("./chr", 1:19, ".LD.10Mb.filtered")
 
 ld.data <- function(file){
     temp <- read.table(file=file, as.is=T, header=T)[1:3] #[c(2,5,7)]
-    temp$dist <- with(temp, temp$BP_B-temp$BP_A)/1000
+    temp$dist <- with(temp, temp$BP_B-temp$BP_A)/1000000
     ld <- cbind.data.frame(temp$dist, temp$R2)
     return(ld)
 }
@@ -27,15 +27,20 @@ linkageD$distbins <- as.numeric(rownames(linkageD))
 
 
 library(ggplot2)
-pdf("/group/palmer-lab/AIL/LgSm-DataProcessing/figures/LD.10Mb.filtered.pdf",
-    height=4, width=4.25)
 LDplot<- ggplot(linkageD)+
     #geom_smooth(aes(x=distbins, y=med2), se=FALSE, color="#ff6040")+
-    geom_point(data=ld.info, aes(x=dist, y=R2), pch=".", color="ivory3")+
+    #geom_point(data=ld.info, aes(x=dist, y=R2), pch=".", color="ivory3")+
     geom_smooth(aes(x=distbins, y=meanr2), se=FALSE, color="black")+
     geom_smooth(aes(x=distbins, y=qt95), se=FALSE, color="slateblue")+
     theme_bw()+
-    xlab("Distance (kB)")+
+    xlab("Distance (Mb)")+
     ylab(expression(paste("Linkage disequilibrium (",r^2,")")))
+
+save(LDplot, file="./LDplotObject.RData")
+
+pdf("/group/palmer-lab/AIL/LgSm-DataProcessing/figures/LD.10Mb.pdf",
+    height=5, width=5.25)
 LDplot
 dev.off()
+
+
