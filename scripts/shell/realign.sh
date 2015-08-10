2d976d522b78f9051af640897965337421bb687f
Gonzales <ngonzales@cri12in02.cri.uchicago.edu>
2015-08-10 17:12:32 -0500

    updated eQTL info from Jung

--- scripts/shell/realign.sh
+++ scripts/shell/realign.sh
@@ -5,13 +5,13 @@
 ########################
 
 ### Set the name of the job, where jobname is a unique name for your job
-#PBS -N Realign
+#PBS -N realign.ail.2
 
 ### Select the shell you would like the script to execute within
 #PBS -S /bin/bash
 
 ### Inform the scheduler of the expected runtime, where walltime=HH:MM:SS
-#PBS -l walltime=24:00:00
+#PBS -l walltime=01:30:00
 
 ### Inform the scheduler of the number of CPU cores for your job.
 ### This example will allocate 1 core on a single node.
@@ -32,10 +32,10 @@
 module load java
 module load gatk/3.3-0
 
-INFILE=`head -$PBS_ARRAYID /group/palmer-lab/AIL/GBS/bam.ail.ctrl.list | tail -1`
+INFILE=`head -$PBS_ARRAYID /group/palmer-lab/AIL/GBS/bams/realign.ail.2.list | tail -1`
 BASE=`basename $INFILE .bam`
 DIR=`dirname $INFILE`
 echo "Running realignment for: $INFILE" 
-java -Xmx2g -jar /apps/software/GenomeAnalysisTK/3.3-0/GenomeAnalysisTK.jar -T IndelRealigner -R /group/palmer-lab/reference_genomes/mouse/mm10.fasta -I $INFILE -targetIntervals /group/palmer-lab/reference_genomes/mouse/LG_SM.mm10.realign.intervals -o $DIR/$BASE.realign.bam
+java -Xmx2g -jar /apps/software/GenomeAnalysisTK/3.3-0/GenomeAnalysisTK.jar -T IndelRealigner -R /group/palmer-lab/reference_genomes/mouse/mm10.fasta -I $INFILE -targetIntervals /group/palmer-lab/reference_genomes/mouse/LG_SM.mm10.realign.intervals -o $DIR/$BASE.ra.bam
 echo "Done running realignment."
 
